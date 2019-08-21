ea6d6e784de73b54cde06b37538546dac182cd65
Lisa Rosenthal <lrosenthal@ucdavis.edu>
2019-08-21 08:13:19 -0700

    made master file

---
+++ rscripts/analysis2019/make_masterfile.R
@@ -0,0 +1,45 @@
+#finalize master file.
+#need to convert lesion samples that were manually done from pixels to %lesion
+#create linear model between area and lesion to figure out %lesion.
+
+setwd("~/Box/Competency project/competency.git/data2019")
+library(dplyr)
+library(ggplot2)
+
+df <- readxl::read_excel('master_bothassays.xlsx')
+#convert random numbers to actual dates
+df$date_counted <- as.Date(as.numeric(df$date_counted), origin = as.Date("1899-12-31"))
+df$perc_lesion <- as.numeric(df$perc_lesion) #character for some reason
+head(df)
+
+#remove samples that were manually done. includes mostly 0 and 100 or near 100. also ones without both total area and perc_lesion
+df2 <- df %>% filter(
+  !(is.na(perc_lesion)|is.na(total_area)),
+  perc_lesion<99)
+head(df2)
+
+#plot the relationship between area and lesion. pretty damn linear.
+ggplot(df2, aes(total_area, perc_lesion))+
+  geom_point()+
+  geom_smooth(method = 'lm')
+m1 <- lm(data=df2, perc_lesion~0+total_area) #0 constrains model to pass through 0 at intercept
+summary(m1)
+
+#create data to predict lesion size
+ndat <- df %>% filter(!is.na(total_area), is.na(perc_lesion))
+ndat$perc_lesion <- predict(m1, newdata = ndat)
+head(ndat)
+
+#plot points with new data
+ggplot(df2, aes(total_area, perc_lesion))+
+  geom_point()+
+  geom_smooth(method = 'lm') +
+  geom_point(data=ndat, aes(total_area, perc_lesion), color='red')
+
+#merge new data with original data
+dfinal <- rbind(ndat, df) %>%  
+  filter(!duplicated(leafID2)) %>% 
+  arrange(leafID)
+#View(dfinal)
+
+write.csv(dfinal, "master_tall.csv", row.names = F)
